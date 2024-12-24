c895323020c1db391a55736362649449454d2245
Qijia Liu <liumeo@pku.edu.cn>
2024-12-24 00:47:35 -0500

    fix zip 404 hangs

--- src/rime.js
+++ src/rime.js
@@ -1,7 +1,12 @@
 export async function loadZip(url) {
   const [_, schema] = await Promise.all([
     fcitxReady,
-    fetch(url).then(res => res.arrayBuffer())
+    fetch(url).then(res => {
+      if (!res.ok) {
+        throw new Error(`Get ${url} error`);
+      }
+      return res.arrayBuffer()
+    })
   ])
   window.fcitx.unzip(schema, '/usr/share/rime-data')
   window.fcitx.enable()
