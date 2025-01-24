8d5f087e20a4ebcf7b3597e81f7abda8908e46ef
Qijia Liu <liumeo@pku.edu.cn>
2025-01-23 19:29:14 -0500

    fix lib dir

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -3,6 +3,6 @@ set -e
 rm -rf dist/*
 mkdir -p dist
 
-cp build/sysroot/usr/lib/libFcitx5* dist
+cp build/fcitx5-js/destdir/usr/lib/libFcitx5* dist
 cp build/fcitx5-js/src/Fcitx5.* dist
 cat fcitx5-js/page/Fcitx5.d.ts src/fcitx5-rime.d.ts > dist/Fcitx5.d.ts
