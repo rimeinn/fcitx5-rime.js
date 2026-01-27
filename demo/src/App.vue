befaf2e275ba70a9a682c67bdfe1927f66ef40b5
Qijia Liu <liumeo@pku.edu.cn>
2026-01-26 21:58:52 -0500

    demo disable system im [skip ci]

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts">
+<script setup>
 import { loadZip } from 'fcitx5-rime'
 
 loadZip('./rime-ice.zip')
+  .then(() => window.fcitx.setSystemInputMethodInUseCallback(() => alert('请禁用系统输入法'))) // 可选
 </script>
 
 <template>
