69860b665fe6966b73a852e00942dd6c51009650
Qijia Liu <liumeo@pku.edu.cn>
2026-09-12 15:30:29 -0400

    fix script

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -5,9 +5,10 @@ pnpm i --prefix=fcitx5-js/fcitx5-webview
 pnpm i --prefix=fcitx5-js/fcitx5-keyboard-web
 pnpm --prefix=fcitx5-js/fcitx5-keyboard-web run build
 
+mkdir -p cache
 ./fcitx5-js/scripts/install-deps.sh
 file=rime.zip
-[[ -f cache/$file ]] || wget -P cache https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/js/$file
+[[ -f cache/$file ]] || curl -fsSLO --output-dir cache https://github.com/fcitx-contrib/fcitx5-plugins/releases/download/js/$file
 unzip -d build/sysroot/usr cache/$file
 
 mkdir -p build/fcitx5-js
